d8d0ee492ab4dbb70ab3bd5d53dd0f7970322d18
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-01-31 12:45:17 +0530

    239. initialized Session in app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = required("express-session");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -20,6 +21,15 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "My Secret", resave: false, saveUninitialized: false })
+);
+// resave: false -> this means that the session will not be saved on every request
+// that is done, so on every response that is sent but only if
+// something changed in the session., this will obviously improve performance and so on.
+
+// saveUninitialized: false -> this will also basically ensure that no session gets saved for a request where
+// it doesn't need to be saved because nothing waschanged about it
 
 app.use((req, res, next) => {
   User.findById("63d8a54ebeb6de9bea159fde")
